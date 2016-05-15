da6b9ad6ee2e76654164a538597adc23ef09ce0e
Nathan Charvy <nathan.charvy@etu.univ-orleans.fr>
2016-05-15 18:26:40 +0200

    feat (AddBookActivity) Ajout de livre dans la bdd rendu fonctionnem

--- app/src/main/java/com/book/android/android_book/AddBookActivity.java
+++ app/src/main/java/com/book/android/android_book/AddBookActivity.java
@@ -1,17 +1,35 @@
 package com.book.android.android_book;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class AddBookActivity extends AppCompatActivity {
+public class AddBookActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button addButton;
+    private LivresBDD livreBdd;
+    private Livre livre;
+    private EditText etTitre, etIsbn, etAuteur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbook);
-
+        livreBdd = new LivresBDD(this);
+        addButton = (Button) findViewById(R.id.add);
+        etTitre = (EditText) findViewById(R.id.titre);
+        etIsbn = (EditText) findViewById(R.id.isbn);
+        etAuteur = (EditText) findViewById(R.id.auteur);
+        addButton.setOnClickListener(this);
         setTitle("Espace ajout de livre");
     }
 
@@ -35,5 +53,18 @@ public class AddBookActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == addButton) {
+            livre = new Livre(1, etIsbn.getText().toString(), etTitre.getText().toString(), etAuteur.getText().toString());
+            livreBdd.open();
+            livreBdd.insertLivre(livre);
+            livre = livreBdd.getFirstLivreWithTitre(etTitre.getText().toString());
+
+            Toast.makeText(this, "Ajout du livre " + livre.getTitre().toString(), Toast.LENGTH_LONG).show();
+            livreBdd.close();
+        }
     }
 }
